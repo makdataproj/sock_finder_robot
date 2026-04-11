5a250b7f9aa9a35c30220da521d91b5a947c0136
makdataproj <alexmkapydev@gmail.com>
2026-04-12 03:20:13 +0800

    include models and modified maintainer

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'yahboom_sock_finder'
+package_name = 'sock_finder_robot'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -22,14 +22,14 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'sock_mask_target_trt_node = yahboom_sock_finder.sock_mask_target_trt_node:main',
-            'target_lock_node = yahboom_sock_finder.target_lock_node:main',
-            'move_to_sock_mask_ik_node = yahboom_sock_finder.move_to_sock_mask_ik_node:main',
-            'serial_fsr_node = yahboom_sock_finder.serial_fsr_node:main',
-            'scan_pickup_sock_ik_node = yahboom_sock_finder.scan_pickup_sock_ik_node:main',
-            'carry_sock_to_person_node = yahboom_sock_finder.carry_sock_to_person_node:main',
-            'patrol_node = yahboom_sock_finder.patrol_node:main',
-            'combined_trt_node = yahboom_sock_finder.combined_trt_node:main',
+            'sock_mask_target_trt_node = yahboom_yolov8.sock_mask_target_trt_node:main',
+            'target_lock_node = yahboom_yolov8.target_lock_node:main',
+            'move_to_sock_mask_ik_node = yahboom_yolov8.move_to_sock_mask_ik_node:main',
+            'serial_fsr_node = yahboom_yolov8.serial_fsr_node:main',
+            'scan_pickup_sock_ik_node = yahboom_yolov8.scan_pickup_sock_ik_node:main',
+            'carry_sock_to_person_node = yahboom_yolov8.carry_sock_to_person_node:main',
+            'patrol_node = yahboom_yolov8.patrol_node:main',
+            'combined_trt_node = yahboom_yolov8.combined_trt_node:main',
         ],
     },
 )
